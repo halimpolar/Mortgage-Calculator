dc299510699c21f8cbc4787308f507f7b9e8ab40
Sterling Tarng <Sterling Tarng>
2017-03-13 16:49:07 -0700

    initial commit

---
+++ app/src/main/java/edu/sjsu/cmpe277/cmpe277lab2/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.sjsu.cmpe277.cmpe277lab2;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
