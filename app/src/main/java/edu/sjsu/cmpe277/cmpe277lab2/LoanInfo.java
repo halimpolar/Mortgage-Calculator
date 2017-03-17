f9241dbc85d1fcd080a1f4556c5e468dc455ef1b
Sterling Tarng <Sterling Tarng>
2017-03-16 17:27:55 -0700

    added LoanInfo getters/setters

--- app/src/main/java/edu/sjsu/cmpe277/cmpe277lab2/LoanInfo.java
+++ app/src/main/java/edu/sjsu/cmpe277/cmpe277lab2/LoanInfo.java
@@ -6,4 +6,41 @@ package edu.sjsu.cmpe277.cmpe277lab2;
  */
 
 public class LoanInfo {
+    private double propertyPrice;
+    private double downPayment;
+    private double apr;
+
+    public int getTerms() {
+        return terms;
+    }
+
+    public void setTerms(int terms) {
+        this.terms = terms;
+    }
+
+    public double getPropertyPrice() {
+        return propertyPrice;
+    }
+
+    public void setPropertyPrice(double propertyPrice) {
+        this.propertyPrice = propertyPrice;
+    }
+
+    public double getDownPayment() {
+        return downPayment;
+    }
+
+    public void setDownPayment(double downPayment) {
+        this.downPayment = downPayment;
+    }
+
+    public double getApr() {
+        return apr;
+    }
+
+    public void setApr(double apr) {
+        this.apr = apr;
+    }
+
+    private int terms;
 }
